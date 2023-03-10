74756c9ff5e07fd50393da95d2bf49b5ce8fa628
HyunCafe <eparksuncafe@gmail.com>
2023-03-09 19:10:34 -0600

    Add: added display result and creategrid through dom

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const body = document.body;
+
 
 // Player 1 Add Event Listener to game board choices on a 3x3 grid
 
@@ -8,9 +10,21 @@
 // Wining condition logic/function
 
 // Winner Display Message
+const displayResult = (result) => {
+    document.getElementsByClassName('main__result').textContent = result;
+}
 
 // Reset Game Function
 
 // Add best of 3 counter
 
 // Create the Board through the DOM
+function createGrid() {
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+            const gridCell = document.createElement('div');
+            gridCell.classList.add('grid-cells');
+            body.append(gridCell)
+        }
+    }
+}
