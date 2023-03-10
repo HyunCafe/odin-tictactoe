233586fc07bf43f14b6294dbd1e96b809a6569a1
HyunCafe <eparksuncafe@gmail.com>
2023-03-09 18:53:00 -0600

    Plan: Starting some pseudo code for the project

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+// Player 1 Add Event Listener to game board choices on a 3x3 grid
+
+// Player 2 computer logic, attempts to block winning conditions after player 1's turn
+
+// Wining condition logic/function
+
+// Winner Display Message
+
+// Reset Game Function
+
+// Add best of 3 counter
+
+// Create the Board through the DOM
