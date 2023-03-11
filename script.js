d770c1e70dbb14c79b9f669baf0218366a1f3423
HyunCafe <eparksuncafe@gmail.com>
2023-03-10 22:03:46 -0600

    Add: Added some basic css styling, optimized grid creation

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const body = document.body;
+const mainGrid = document.querySelector('.main__game')
 
 
 // Player 1 Add Event Listener to game board choices on a 3x3 grid
@@ -19,12 +19,39 @@ const displayResult = (result) => {
 // Add best of 3 counter
 
 // Create the Board through the DOM
-function createGrid() {
-    for (let row = 0; row < 3; row++) {
-        for (let col = 0; col < 3; col++) {
-            const gridCell = document.createElement('div');
-            gridCell.classList.add('grid-cells');
-            body.append(gridCell)
-        }
+const createGrid = (rows, cols) => {
+    const mainGrid = document.querySelector('.main__game');
+    for (let row = 0; row < rows; row++) {
+      for (let col = 0; col < cols; col++) {
+        const gridCell = document.createElement('div');
+        gridCell.classList.add('grid-cells');
+        mainGrid.append(gridCell);
+      }
     }
-}
+  };
+  createGrid(3, 3);
+  
+  
+  // Define rules object
+  const RULES = {
+    players: {
+        PLAYER_ONE: 'X',
+        PLAYER_TWO: 'O'
+    }
+  };
+  
+
+  // Computer Logic for Challenging where its optimal logic and Impossible where it cheats
+  // in a goofy way, the logic will be easy so it lets player think they can win
+  // and right before they win, a gif of sweating will come up from the computer
+  // and the computer will cheat and win and say was a close one in some cheeky way
+
+
+  // Add event listeners to player choices
+  const playerChoices = document.querySelectorAll('');
+  playerChoices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+     // will fill logic in
+    });
+  });
+  
