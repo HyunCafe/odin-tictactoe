61abb559713f97dac82a7513e4d7f89ed1ecac11
HyunCafe <eparksuncafe@gmail.com>
2023-03-14 02:10:27 -0500

    Add:Reorganized Code, and added Comment Sections, also  Added Hard Mode and Reset

--- script.js
+++ script.js
@@ -2,19 +2,7 @@
 
 const mainGrid = document.querySelector(".main__game");
 
-// Define rules object
-const RULES = {
-  players: {
-    PLAYER_ONE: "X",
-    PLAYER_TWO: "O",
-  },
-};
-
-const MODES = {
-  EASY: "easy",
-  MEDIUM: "medium",
-  HARD: "hard",
-};
+// ----------------------- Display and Track Section  ----------------------- //
 
 // Winner Display Message
 const displayResult = (result) => {
@@ -47,10 +35,12 @@ const trackResult = () => {
   document.querySelector(".ties").textContent = `Ties ${ties}`;
 };
 
+// ----------------------- Create Grid Section  ----------------------- //
+
 // Create the Board through the DOM
 const createGrid = (rows, cols) => {
   const mainGrid = document.querySelector(".main__game");
-  
+
   // Check if the grid cells have already been created and appended
   if (mainGrid.childElementCount === 0) {
     let cellIndex = 0;
@@ -69,6 +59,8 @@ const createGrid = (rows, cols) => {
 };
 
 createGrid(3, 3);
+
+// ----------------------- Player Event Listener Section  ----------------------- //
 
 // Player 1 Add Event Listener to game board choices on a 3x3 grid
 const cells = document.querySelectorAll(".grid-cells");
@@ -108,7 +100,7 @@ cells.forEach((cell) => {
       }
     });
 
-    easyComputerMove();
+    computerMove();
 
     if (moves >= 5) {
       checkGameState();
@@ -116,7 +108,69 @@ cells.forEach((cell) => {
   });
 });
 
+// ----------------------- Game Logic Section  ----------------------- //
+
+// Define rules object
+const RULES = {
+  players: {
+    PLAYER_ONE: "X",
+    PLAYER_TWO: "O",
+  },
+};
+
+// Set player symbols
+const playerOneSymbol = RULES.players.PLAYER_ONE;
+const playerTwoSymbol = RULES.players.PLAYER_TWO;
+
+const MODES = {
+  EASY: "easy",
+  HARD: "hard",
+  IMPOSSIBLE: "impossible",
+};
+
+// Get mode buttons
+const easyBtn = document.querySelector(".button-easy");
+const hardBtn = document.querySelector(".button-hard");
+const impossibleBtn = document.querySelector(".button-impossible");
+
+// Add event listeners to buttons
+easyBtn.addEventListener("click", () => {
+  currentMode = MODES.EASY;
+  console.log("Current mode set to Easy");
+});
+
+hardBtn.addEventListener("click", () => {
+  currentMode = MODES.HARD;
+  console.log("Current mode set to Hard");
+});
+
+impossibleBtn.addEventListener("click", () => {
+  currentMode = MODES.IMPOSSIBLE;
+  console.log("Current mode set to Impossible");
+});
+
+const getAvailableCells = () => {
+  return [...document.querySelectorAll(".grid-cells")].filter(
+    (cell) => !cell.getAttribute("data-selected")
+  );
+};
+
+// Use the currentMode variable to determine the computer move logic
+const computerMove = () => {
+  if (currentMode === MODES.EASY) {
+    console.log("Using Easy Mode computer move logic");
+    easyComputerMove();
+  } else if (currentMode === MODES.HARD) {
+    console.log("Using Hard Mode computer move logic");
+    hardComputerMove();
+  } else if (currentMode === MODES.IMPOSSIBLE) {
+    console.log("Using Impossible Mode computer move logic");
+    impossibleComputerMove();
+  }
+};
+
 // Computer Logic Easy Mode
+let currentMode = MODES.EASY;
 const easyComputerMove = () => {
   if (availableCells.length === 0) {
     return;
@@ -134,6 +188,132 @@ const easyComputerMove = () => {
         const randomIndex = Math.floor(Math.random() * availableCells.length);
         computerChoice = availableCells[randomIndex];
       } while (computerChoice.getAttribute("data-selected") === "true");
+
+      // Check if the computer can win with the next move
+      const nextMoveWins = winningCombinations.some((combination) => {
+        const cells = combination.map((index) =>
+          document.querySelector(`[data-index="${index}"]`)
+        );
+        const computerCells = cells.filter(
+          (cell) =>
+            cell.getAttribute("data-selected") === "true" &&
+            cell.textContent === "O"
+        );
+        const emptyCell = cells.find(
+          (cell) => cell.getAttribute("data-selected") !== "true"
+        );
+
+        if (computerCells.length === 2 && emptyCell) {
+          computerChoice = emptyCell;
+          return true;
+        }
+        return false;
+      });
+
+      // Check if the player can win with the next move and block it
+      if (!nextMoveWins) {
+        winningCombinations.some((combination) => {
+          const cells = combination.map((index) =>
+            document.querySelector(`[data-index="${index}"]`)
+          );
+          const playerCells = cells.filter(
+            (cell) =>
+              cell.getAttribute("data-selected") === "true" &&
+              cell.textContent === "X"
+          );
+          const emptyCell = cells.find(
+            (cell) => cell.getAttribute("data-selected") !== "true"
+          );
+
+          if (playerCells.length === 2 && emptyCell) {
+            computerChoice = emptyCell;
+            return true;
+          }
+          return false;
+        });
+      }
+    }
+
+    const oSpan = document.createElement("span");
+    oSpan.textContent = "O";
+    oSpan.classList.add("grid-text");
+    computerChoice.append(oSpan);
+    computerChoice.setAttribute("data-selected", true);
+    moves++;
+
+    if (moves >= 5) {
+      checkGameState();
+    }
+
+    isComputerMoving = false;
+  }, 600);
+};
+
+// Computer Logic Hard Mode
+const hardComputerMove = () => {
+  if (availableCells.length === 0) {
+    return;
+  }
+
+  isComputerMoving = true;
+  setTimeout(() => {
+    if (checkGameState()) {
+      return;
+    }
+
+    let computerChoice;
+
+    // Check if the computer can win with the next move
+    const nextMoveWins = winningCombinations.some((combination) => {
+      const cells = combination.map((index) =>
+        document.querySelector(`[data-index="${index}"]`)
+      );
+      const computerCells = cells.filter(
+        (cell) =>
+          cell.getAttribute("data-selected") === "true" &&
+          cell.textContent === "O"
+      );
+      const emptyCell = cells.find(
+        (cell) => cell.getAttribute("data-selected") !== "true"
+      );
+
+      if (computerCells.length === 2 && emptyCell) {
+        computerChoice = emptyCell;
+        return true;
+      }
+      return false;
+    });
+
+    // Check if the player can win with the next move and block it
+    if (!nextMoveWins) {
+      const blockingMoves = [];
+      winningCombinations.forEach((combination) => {
+        const cells = combination.map((index) =>
+          document.querySelector(`[data-index="${index}"]`)
+        );
+        const playerCells = cells.filter(
+          (cell) =>
+            cell.getAttribute("data-selected") === "true" &&
+            cell.textContent === "X"
+        );
+        const emptyCell = cells.find(
+          (cell) => cell.getAttribute("data-selected") !== "true"
+        );
+
+        if (playerCells.length === 2 && emptyCell) {
+          blockingMoves.push(emptyCell);
+        }
+      });
+
+      if (blockingMoves.length > 0) {
+        // Choose a random blocking move
+        const randomIndex = Math.floor(Math.random() * blockingMoves.length);
+        computerChoice = blockingMoves[randomIndex];
+      } else {
+        // Choose a random move
+        const randomIndex = Math.floor(Math.random() * availableCells.length);
+        computerChoice = availableCells[randomIndex];
+      }
     }
 
     const oSpan = document.createElement("span");
@@ -152,7 +332,16 @@ const easyComputerMove = () => {
 };
 
 // Winner Condition Logic
-const winningCombinations = [];
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 // rows
 for (let i = 0; i < 9; i += 3) {
@@ -221,6 +410,8 @@ const checkWinningCondition = (player) => {
   return isWinning;
 };
 
+// ----------------------- Others Section  ----------------------- //
+
 // Reset Game Function
 const resetGame = () => {
   const gridCells = document.querySelectorAll(".grid-cells");
@@ -232,7 +423,22 @@ const resetGame = () => {
   isUpdated = false;
   isComputerMoving = false;
   availableCells.length = 0;
+
+  // Set current computer mode to previous value
+  const modeRadios = document.querySelectorAll('input[name="mode"]');
+  let selectedMode = MODES.EASY; 
+  for (let i = 0; i < modeRadios.length; i++) {
+    if (modeRadios[i].checked) {
+      selectedMode = modeRadios[i].value;
+      break;
+    }
+  }
+  currentMode = selectedMode;
 };
+
+// Reset button event listener
+const resetButton = document.querySelector(".button-reset");
+resetButton.addEventListener("click", resetGame);
 
 //TODO:
 
